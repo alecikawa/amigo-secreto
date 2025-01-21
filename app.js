5742aeaeceeac126ec8a419ec6ba4108216e5210
Alec Ikawa <alec.ikawa.almeida@outlook.com>
2025-01-21 10:29:49 -0300

    att

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// declarando recursos utilizados
+const inputNomeInserido = document.getElementById('amigo');
+const ulListaAmigos = document.getElementById('listaAmigos');
+const ulResultado = document.getElementById('resultado');
+let nomes = [];
+
+// função para adicionar um amigo
+function agregarAmigo() {
+    if(ulResultado.innerHTML !== '') {
+        ulResultado.innerHTML = '';
+    }
+
+    if(inputNomeInserido.value === '') {
+        alert('Informe o nome do amigo');
+    }else if(nomes.includes(inputNomeInserido.value)) {
+        alert('Amigo já adicionado');
+    }else{
+        nomes.push(inputNomeInserido.value)
+    }
+
+    atualizarLista();
+
+    inputNomeInserido.value = '';
+}
+
+function atualizarLista() {
+    ulListaAmigos.innerHTML = '';
+
+    for(let i = 0; i < nomes.length; i++) {
+        const li = document.createElement('li');
+
+        li.innerHTML = nomes[i];
+
+        ulListaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    const indiceAleatorio = Math.floor((Math.random() * nomes.length)); 
+
+    if(nomes.length === 0) {
+        alert('lista vazia');
+    }else{
+        const li = document.createElement('li');
+        
+        ulListaAmigos.innerHTML = '';
+
+        li.innerHTML = `Nome Sorteado: ${nomes[indiceAleatorio]}`;
+    
+        ulListaAmigos.appendChild(li);
+    }
+
+    nomes = []; 
+}
